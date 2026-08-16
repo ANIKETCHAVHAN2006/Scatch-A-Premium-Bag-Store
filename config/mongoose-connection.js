f8918f65e45864c08b0d610804af054365fce4c9
ANIKETCHAVHAN2006 <aniketchavhan991@gmail.com>
2026-08-16 19:37:42 +0530

    Development/Production setup

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
+const config = require('config');
+const dbgr = require("debug")("development:mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/scatch')
+mongoose
+.connect(`${config.get("MONGODB_URI")}/scatch-a-premium-bag-store`)
 .then(() => {
-    console.log('Connected to MongoDB');
+    dbgr('Connected to MongoDB');
 })
 .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
+    dbgr(err);
 });
 
 module.exports = mongoose.connection;
